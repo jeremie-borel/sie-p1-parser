f7e8ae36f39190f2ea39e32a65d01e76b67c5847
Jeremie <jeremie@aio.li>
2024-02-17 21:50:47 +0100

    wip

--- p1parser/p1reader.py
+++ p1parser/p1reader.py
@@ -5,7 +5,6 @@ import logging
 import datetime
 import serial
 import time
-from collections import namedtuple
 
 from dlms_cosem.cosem import Obis
 from dlms_cosem.hdlc.frames import UnnumberedInformationFrame
@@ -13,34 +12,10 @@ from dlms_cosem.hdlc.exceptions import HdlcParsingError
 from dlms_cosem.protocol.xdlms.data_notification import DataNotification
 from dlms_cosem.utils import parse_as_dlms_data
 
-TimedResults = namedtuple('TimedResults', 'datetime results')
-
 # flag wrapping hdlc frames
 flag_char = bytes.fromhex('7e')
 
 log = logging.getLogger(__name__)
-
-
-_map = [
-    ('020309060100010700ff06', 'input_power', 'W'),
-    ('020309060100020700ff06', 'output_power', 'W'),
-    ('020309060100200700ff12', 'voltage1', 'V'),
-    ('020309060100340700ff12', 'voltage2', 'V'),
-    ('020309060100480700ff12', 'voltage3', 'V'),
-    ('0203090601001f0700ff12', 'current1', 'A'),
-    ('020309060100330700ff12', 'current2', 'A'),
-    ('020309060100470700ff12', 'current3', 'A'),
-    ('020309060100010801ff06', 'energy_import_tarif_plein', 'Wh'),
-    ('020309060100010802ff06', 'energy_import_tarif_creux', 'Wh'),
-    ('020309060100020801ff06', 'energy_export_tarif_plein', 'Wh'),
-    ('020309060100020802ff06', 'energy_export_tarif_plein', 'Wh'),
-]
-
-def get_map() -> list[tuple[str, str]]:
-    return [
-        (name, unit)
-        for _, name, unit in _map
-    ]
 
 
 class SieP1Reader:
@@ -134,8 +109,10 @@ def main():
 
     reader = SieP1Reader()
     log.info("Hit Ctrl+C to stop the script")
-    for data in reader.read():
-        print('data:', data)
+    for dt, data in reader.read():
+        print("Time is ", dt)
+        for item in data:
+            print(item)
 
 
 
