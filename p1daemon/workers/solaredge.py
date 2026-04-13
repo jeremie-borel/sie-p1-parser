d365a0bd5b2e336f0c77c41ea6af70820b79a5a1
Jeremie <jeremie@aio.li>
2026-04-13 17:57:31 +0200

    fixed some configs

--- p1daemon/workers/solaredge.py
+++ p1daemon/workers/solaredge.py
@@ -5,26 +5,21 @@ import logging
 
 from urllib3.exceptions import InsecureRequestWarning
 from urllib3 import disable_warnings
+
 disable_warnings(InsecureRequestWarning)
 
-from ..config import (
-    SOLAREDGE_API,
-    SOLAREDGE_ID,
-    EVCC_KEY,
-)
-from ..stats import (
-    _zurich,
-    as_datetime,
-)
+from ..config import SOLAREDGE_API, SOLAREDGE_ID, EVCC_KEY
+from ..stats import _zurich, as_datetime
 
 from multiprocessing import Process
 
-MIN_QUERY_INTERVAL = 15*60 # min time between updates of SE value.
-QUERY_TIME_RANGE = (5,20)
+MIN_QUERY_INTERVAL = 15 * 60  # min time between updates of SE value.
+QUERY_TIME_RANGE = (5, 20)
 
 log = logging.getLogger(__name__)
 
-def query_solaredge(time_unit: str = 'QUARTER_OF_AN_HOUR') -> dict:
+
+def query_solaredge(time_unit: str = "QUARTER_OF_AN_HOUR") -> dict:
     """
     Query solar edge data. Either power of energy for the given time
     periode. Max diff between :start: and :end: is one month.
@@ -33,15 +28,16 @@ def query_solaredge(time_unit: str = 'QUARTER_OF_AN_HOUR') -> dict:
 
     url = f"https://monitoringapi.solaredge.com/site/{SOLAREDGE_ID}/powerDetails.json"
     kwargs = {
-        'api_key': SOLAREDGE_API,
-        'startTime': as_datetime(now - datetime.timedelta(minutes=15)),
-        'endTime': as_datetime(now),
-        'timeUnit': time_unit,
-        'meters': "PRODUCTION"
+        "api_key": SOLAREDGE_API,
+        "startTime": as_datetime(now - datetime.timedelta(minutes=15)),
+        "endTime": as_datetime(now),
+        "timeUnit": time_unit,
+        "meters": "PRODUCTION",
     }
     r = requests.get(url, params=kwargs, verify=False)
     r.raise_for_status()
     return r.json()
+
 
 def query_power_value() -> float:
     try:
@@ -51,7 +47,7 @@ def query_power_value() -> float:
         log.exception(e)
         return 0.0
     try:
-        item = data['powerDetails']['meters'][0]['values'][-1]
+        item = data["powerDetails"]["meters"][0]["values"][-1]
     except Exception as e:
         log.error(f"Got an exception while querying solaredge server")
         log.exception(e)
@@ -60,8 +56,9 @@ def query_power_value() -> float:
     # specifyied timerange
     if "value" in item:
         return round(float(item["value"]), 3)
-    
+
     return 0.0
+
 
 class SolarEdgeWorker(Process):
     def __init__(self, shared_dict: dict):
@@ -83,13 +80,13 @@ class SolarEdgeWorker(Process):
             else:
                 log.info("It's night return 0 solar power.")
                 value = 0.0
-            
+
             now = datetime.datetime.now(tz=_zurich)
-            copy = {k:v for k,v in self.data.get(EVCC_KEY,{}).items()}
-            copy['solar_power'] = value
-            copy['solar_stamp'] = as_datetime(now)
+            copy = {k: v for k, v in self.data.get(EVCC_KEY, {}).items()}
+            copy["solar_power"] = value
+            copy["solar_stamp"] = as_datetime(now)
             self.data[EVCC_KEY] = copy
-            
+
             time.sleep(MIN_QUERY_INTERVAL)
 
 
@@ -97,5 +94,6 @@ def main():
     print("Get SE solar power")
     print(query_power_value())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
